a944534f2c365d1075e5dcdab690d4949e4a40b8
htharwani <harshatharwani7@gmail.com>
2025-12-15 14:22:01 +0530

    second

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+hyphen_e= '-e .'
+def get_requirements(file_path:str) ->List[str]:
+    '''this function will return the list of requirements
+ '''
+    requirements=[]
+    with open (file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", " ") for req in requirements]
+        if hyphen_e in requirements:
+            requirements.remove(hyphen_e)
+
+
+
+setup(
+name= "ml-projects",
+version='0.0.1',
+author='Harsha',
+author_email= "harshatharwani7@gmail.com",
+packages=find_packages(),
+install_requires=['pandas', 'numpy', 'seaborn']
+
+
+
+)
